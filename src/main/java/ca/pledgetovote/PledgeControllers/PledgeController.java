b63f508858c407806cd9a4ea9929e5f023f49546
TheCodingStache <cs4415161@teilar.gr>
2019-10-23 14:38:02 +0300

    Covers creating a controller for a Rest API for GET and PUT requests,plus handling exceptions.

---
+++ src/main/java/ca/pledgetovote/PledgeControllers/PledgeController.java
@@ -0,0 +1,30 @@
+package ca.pledgetovote.PledgeControllers;
+
+import ca.pledgetovote.model.Pledge;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class PledgeController {
+    private List<Pledge> pledges = new ArrayList<>();
+    private AtomicLong nextID = new AtomicLong();
+
+    @GetMapping("/hello")
+    public String getHelloMessage() {
+        return "Hello Springboot Java";
+    }
+
+    @PostMapping("/pledges")
+    public Pledge createNewPledge(@RequestBody Pledge pledge) {
+        //Set pledge to have next ID
+        pledge.setId(nextID.incrementAndGet());
+        pledges.add(pledge);
+        return pledge;
+    }
+}
---
+++ src/main/java/ca/pledgetovote/model/Pledge.java
@@ -0,0 +1,41 @@
+package ca.pledgetovote.model;
+
+public class Pledge {
+    private long id;
+    private String name;
+    private String statement;
+
+    public Pledge() {
+
+    }
+
+    public Pledge(long id, String name, String statement) {
+        this.id = id;
+        this.name = name;
+        this.statement = statement;
+    }
+
+    public void setStatement(String statement) {
+        this.statement = statement;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatement() {
+        return statement;
+    }
+}
